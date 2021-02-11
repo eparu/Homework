a111597e0d07ccc0f9b8408cc509ab86259bf5c8
eparu <kat.pa@bk.ru>
2021-02-11 22:37:49 +0300

    add 4 folder

---
+++ 1week/4/4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  4
+//
+//  Created by Kate on 11.02.2021.
+//  Copyright © 2021 Kate. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
