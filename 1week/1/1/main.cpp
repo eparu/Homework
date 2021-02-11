7987b8bc633402a61fde401ec5ba2703ef86f9ae
eparu <kat.pa@bk.ru>
2021-02-11 22:12:06 +0300

    add other file from 1 folder

---
+++ 1week/1/1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by Kate on 11.02.2021.
+//  Copyright © 2021 Kate. All rights reserved.
+//
+//
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
