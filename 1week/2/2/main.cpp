5c106ae09c92e9f24c2ef4e712612ebdb09e9485
eparu <kat.pa@bk.ru>
2021-02-15 23:52:43 +0300

    Commit in New branch

--- 1week/2/2/main.cpp
+++ 1week/2/2/main.cpp
@@ -4,12 +4,16 @@
 //
 //  Created by Kate on 11.02.2021.
 //  Copyright © 2021 Kate. All rights reserved.
-//
+// ==========================
 
 #include <iostream>
 
+int g(int a);
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    std::cout << "string\n";
     std::cout << "Hello, World!\n";
     return 0;
 }
