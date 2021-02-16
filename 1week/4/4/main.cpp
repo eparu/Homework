6c458c4770ddca433a82be2ef3b6f5680a71c608
eparu <kat.pa@bk.ru>
2021-02-16 21:13:56 +0300

    Added 4 task

--- 1week/4/4/main.cpp
+++ 1week/4/4/main.cpp
@@ -1,15 +1,95 @@
-//
-//  main.cpp
-//  4
-//
-//  Created by Kate on 11.02.2021.
-//  Copyright © 2021 Kate. All rights reserved.
-// ======================
-
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Timer
+{
+public:
+    using clock_t = std::chrono::steady_clock;
+    using time_point_t = clock_t::time_point;
+    using duration_t = clock_t::duration;
+    Timer(const std::string& timer_name = "Untitled") : m_timer_name(timer_name), is_timer_work(true), m_time(duration_t::zero()), m_begin(clock_t::now()) {};
+    ~Timer()
+    {
+        is_timer_work = false;
+        std::cout << "Delete ";
+        Print();
+    }
+    void Stop()
+    {
+        if (is_timer_work)
+        {
+            is_timer_work = false;
+            m_time += (clock_t::now() - m_begin);
+        }
+    }
+    void Continue()
+    {
+        if (!is_timer_work)
+        {
+            is_timer_work = true;
+            m_begin = clock_t::now();
+        }
+    }
+    void Reset()
+    {
+        is_timer_work = true;
+        m_time = duration_t::zero();
+        m_begin = clock_t::now();
+
+    }
+    void Print()
+    {
+        auto timer_worked = is_timer_work;
+        Stop();
+        std::cout << m_timer_name << " : "<< std::chrono::duration_cast<
+        std::chrono::microseconds> (m_time).count() << " microseconds\n";
+        if (timer_worked)
+        {
+            Continue();
+        }
+    }
+private:
+    const std::string m_timer_name;
+    bool is_timer_work;
+    duration_t m_time;
+    time_point_t m_begin;
+};
+
+
+int main() {
+    Timer all_time("Time_since_begin");
+    std::vector<int> v(1000000);
+    for (auto  item : v)
+    {
+        item = rand();
+    }
+
+    Timer all_sorts("Time_of_all_sorts");
+    Timer t("One_sort"); // timer for v1
+    std::sort(std::begin(v), std::end(v));
+    t.Stop();
+    all_sorts.Stop();
+
+    t.Print();
+    all_sorts.Print();
+
+
+    for (auto  item : v)
+    {
+        item = rand();
+    }
+
+    all_sorts.Continue();
+    t.Reset();
+    std::sort(std::begin(v), std::end(v));
+    t.Stop();
+    all_sorts.Stop();
+    t.Print();
+    all_sorts.Print();
+
+
+    all_time.Stop();
     return 0;
 }
