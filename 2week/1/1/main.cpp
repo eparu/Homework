a5a161257eb4fd001807d1dc0b15a118cf6a77ad
eparu <kat.pa@bk.ru>
2021-02-18 12:13:30 +0300

    1 task added

---
+++ 2week/1/1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by Kate on 18.02.2021.
+//  Copyright © 2021 Kate. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
