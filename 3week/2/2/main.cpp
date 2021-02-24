29a0d55215971df17f94e8a69fc39d36d5f45677
eparu <kat.pa@bk.ru>
2021-02-24 22:05:38 +0300

    2 task added

--- 3week/2/2/main.cpp
+++ 3week/2/2/main.cpp
@@ -1,15 +1,30 @@
-//
-//  main.cpp
-//  2
-//
-//  Created by Kate on 21.02.2021.
-//  Copyright © 2021 Kate. All rights reserved.
-//
-
 #include <iostream>
+constexpr int H = 163;
+
+/*
+ 1. Детерминированность: одному и тому же числу соответствует
+ одно и то же возвращаемое значение (нет рандомности).
+ 2. Скорость вычисления не зависит от количества элементов,
+ которые уже есть в хэш-таблице.
+ 3. Функция распределяет более менее равномерно ( для чисел
+ в диапазоне от е-4 до е+4) за счет приведения к целому числу
+ */
+
+std::size_t hash(const double key)
+{
+    int answer = 0;
+    for (int h = key * 10e4 ; h > 0; h /= 10)
+    {
+        answer = answer * 31 + h % 10;
+    }
+
+    return answer % H;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << hash(1.92) << std::endl;
+    std::cout << hash(1.52e-4) << std::endl;
+    std::cout << hash(1.52e4) << std::endl;
+
     return 0;
 }
