1ee1b7c3067e6d0b82dc7b0ce6f6a9d69c18b0c8
eparu <kat.pa@bk.ru>
2021-02-25 19:58:20 +0300

    5 added

--- 3week/5/5/main.cpp
+++ 3week/5/5/main.cpp
@@ -21,6 +21,8 @@ using persons_multi_index = multi_index_container < Person, indexed_by<
     hashed_non_unique < member < Person, std::string, &Person::name > >
 >>;
 
+
+
 int main(int argc, char ** argv)
 {
     persons_multi_index phonebook;
@@ -28,15 +30,14 @@ int main(int argc, char ** argv)
     phonebook.insert({ "A",    1234 });
     phonebook.insert({ "B",    6789 });
     phonebook.insert({ "C",  101112 });
+    phonebook.insert({ "A", 3329 });
+    phonebook.insert({ "B", 8909 });
+    phonebook.insert({ "B", 7909 });
 
 
     auto & tipografie = phonebook.get < 0 > ();
     auto & agency = phonebook.get < 1 > ();
     auto & customer = phonebook.get < 2 > ();
-
-    phonebook.insert({ "A", 3329 });
-    phonebook.insert({ "B", 8909 });
-    phonebook.insert({ "B", 7909 });
 
 
 
@@ -47,6 +48,7 @@ int main(int argc, char ** argv)
         std::cout << "  " << i.name << ' ' << i.phone << std::endl;
     }
 
+
     std::cout << "\nAdvert agency:\n";
     for (const auto & i : agency)
     {
@@ -54,8 +56,39 @@ int main(int argc, char ** argv)
         std::cout << "  " << i.name << ' ' << i.phone << std::endl;
     }
 
-    auto begin = customer.lower_bound("A");
-    auto end   = customer.upper_bound("A");
 
+    std::cout << "Find phone numbers B:\n";
+    auto iterator = customer.equal_range("B");
+    for (auto i = iterator.first; i != iterator.second; i++)
+    {
+        std::cout << "  " << i->name << " " << i->phone << std::endl;
+    }
+
+
+    std::cout << "Delete B 7909:\n";
+    auto iterator_to_delete = customer.equal_range("B");
+    for (auto i = iterator_to_delete.first; i != iterator_to_delete.second; i++)
+    {
+        if (i->phone == 7909)
+        {
+            customer.erase(i);
+            break;
+        }
+    }
+    for (const auto & i : tipografie)
+    {
+
+        std::cout << "  " << i.name << ' ' << i.phone << std::endl;
+    }
+
+
+    std::cout << "Delete all B\n";
+    auto iterator_to_delete_all = customer.equal_range("B");
+    customer.erase(iterator_to_delete_all.first, iterator_to_delete_all.second);
+    for (const auto & i : tipografie)
+    {
+
+        std::cout << "  " << i.name << ' ' << i.phone << std::endl;
+    }
     return 0;
 }
