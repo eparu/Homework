d2035276d0d110ea8c7fcea5978bd081f7db5b7a
eparu <kat.pa@bk.ru>
2021-02-25 12:03:22 +0300

    2 task added

--- 3week/2/2/main.cpp
+++ 3week/2/2/main.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 constexpr int H = 163;
 
@@ -6,25 +7,33 @@ constexpr int H = 163;
  одно и то же возвращаемое значение (нет рандомности).
  2. Скорость вычисления не зависит от количества элементов,
  которые уже есть в хэш-таблице.
- 3. Функция распределяет более менее равномерно ( для чисел
- в диапазоне от е-4 до е+4) за счет приведения к целому числу
+ 3. Функция распределяет более менее равномерно за счет
+ использования битов числа double
  */
 
 std::size_t hash(const double key)
 {
-    int answer = 0;
-    for (int h = key * 10e4 ; h > 0; h /= 10)
-    {
-        answer = answer * 31 + h % 10;
-    }
-
+    auto b = std::bitset <64> (*(unsigned long long*)(&key));
+    b = ((b >> 13) ^ (b << 17) ^ (b >> 31) ^ (b << 3));
+    unsigned long long answer = std::bitset < 64 > (b).to_ullong();
     return answer % H;
 }
 
 int main(int argc, const char * argv[]) {
     std::cout << hash(1.92) << std::endl;
+    std::cout << hash(1.91) << std::endl;
+    std::cout << hash(-1.92) << std::endl;
+
+    std::cout << hash(8.0) << std::endl;
+    std::cout << hash(1.62e+8) << std::endl;
+    std::cout << hash(-1.62e+308) << std::endl;
+     std::cout << hash(1.48e-300) << std::endl;
+
     std::cout << hash(1.52e-4) << std::endl;
-    std::cout << hash(1.52e4) << std::endl;
+    std::cout << hash(-1.52e-4) << std::endl;
+    std::cout << hash(1.51e-4) << std::endl;
+    std::cout << hash(1.52e-3) << std::endl;
+
 
     return 0;
 }
