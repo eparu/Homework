e90071bf1929ad58ac288cc948058bccc3a72804
eparu <kat.pa@bk.ru>
2021-02-25 19:57:48 +0300

    4 added

--- 3week/4/4/main.cpp
+++ 3week/4/4/main.cpp
@@ -1,15 +1,201 @@
-//
-//  main.cpp
-//  4
-//
-//  Created by Kate on 25.02.2021.
-//  Copyright © 2021 Kate. All rights reserved.
-//
-
+#include <chrono>
 #include <iostream>
+#include <functional>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "make_new_words.hpp"
+
+constexpr std::size_t SIZE = 1000;
+
+unsigned int RSHash(const char* str, unsigned int length);
+unsigned int JSHash(const char* str, unsigned int length);
+unsigned int PJWHash(const char* str, unsigned int length);
+unsigned int ELFHash(const char* str, unsigned int length);
+unsigned int BKDRHash(const char* str, unsigned int length);
+unsigned int SDBMHash(const char* str, unsigned int length);
+unsigned int DJBHash(const char* str, unsigned int length);
+unsigned int DEKHash(const char* str, unsigned int length);
+unsigned int APHash(const char* str, unsigned int length);
+
+
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector <std::function <unsigned int(const char*, unsigned int )>> f = {&RSHash, &JSHash, &PJWHash, &ELFHash, &BKDRHash, &SDBMHash, &DJBHash, &DEKHash, &APHash};
+    std::vector<std::string> functions_names = {"RSHash", "JSHash", "PJWHash", "ELFHash","BKDRHash", "SDBMHash", "DJBHash", "DEKHash", "APHash"};
+    int k = 0;
+    for (auto func : f)
+    {
+
+        std::set < std::string > words = make_random_words(SIZE);
+        std::set < std::size_t > hash_string;
+        unsigned int counter_collisions = 0;
+
+        std::cout << functions_names[k++] << std::endl;
+
+
+        for (const auto& i : words)
+        {
+            auto ans = func(i.c_str(), i.size());
+            auto added = hash_string.insert(ans).second;
+            //std::cout << "string ans << std::endl;
+            if (!added) counter_collisions++;
+        }
+        std::cout << counter_collisions << std::endl;
+    }
     return 0;
+}
+
+
+unsigned int RSHash(const char* str, unsigned int length)
+{
+    unsigned int b    = 378551;
+    unsigned int a    = 63689;
+    unsigned int hash = 0;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = hash * a + (*str);
+        a    = a * b;
+    }
+
+    return hash;
+}
+
+
+unsigned int JSHash(const char* str, unsigned int length)
+{
+    unsigned int hash = 1315423911;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash ^= ((hash << 5) + (*str) + (hash >> 2));
+    }
+
+    return hash;
+}
+
+
+unsigned int PJWHash(const char* str, unsigned int length)
+{
+    const unsigned int BitsInUnsignedInt = (unsigned int)(sizeof(unsigned int) * 8);
+    const unsigned int ThreeQuarters     = (unsigned int)((BitsInUnsignedInt  * 3) / 4);
+    const unsigned int OneEighth         = (unsigned int)(BitsInUnsignedInt / 8);
+    const unsigned int HighBits          =
+    (unsigned int)(0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+    unsigned int hash = 0;
+    unsigned int test = 0;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = (hash << OneEighth) + (*str);
+
+        if ((test = hash & HighBits) != 0)
+        {
+            hash = (( hash ^ (test >> ThreeQuarters)) & (~HighBits));
+        }
+    }
+
+    return hash;
+}
+
+
+unsigned int ELFHash(const char* str, unsigned int length)
+{
+    unsigned int hash = 0;
+    unsigned int x    = 0;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = (hash << 4) + (*str);
+
+        if ((x = hash & 0xF0000000L) != 0)
+        {
+            hash ^= (x >> 24);
+        }
+
+        hash &= ~x;
+    }
+
+    return hash;
+}
+
+
+unsigned int BKDRHash(const char* str, unsigned int length)
+{
+    unsigned int seed = 131; /* 31 131 1313 13131 131313 etc.. */
+    unsigned int hash = 0;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = (hash * seed) + (*str);
+    }
+
+    return hash;
+}
+
+
+unsigned int SDBMHash(const char* str, unsigned int length)
+{
+    unsigned int hash = 0;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = (*str) + (hash << 6) + (hash << 16) - hash;
+    }
+
+    return hash;
+}
+
+
+unsigned int DJBHash(const char* str, unsigned int length)
+{
+    unsigned int hash = 5381;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = ((hash << 5) + hash) + (*str);
+    }
+
+    return hash;
+}
+
+
+unsigned int DEKHash(const char* str, unsigned int length)
+{
+    unsigned int hash = length;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash = ((hash << 5) ^ (hash >> 27)) ^ (*str);
+    }
+
+    return hash;
+}
+
+
+unsigned int APHash(const char* str, unsigned int length)
+{
+    unsigned int hash = 0xAAAAAAAA;
+    unsigned int i    = 0;
+
+    for (i = 0; i < length; ++str, ++i)
+    {
+        hash ^= ((i & 1) == 0) ? (  (hash <<  7) ^ (*str) * (hash >> 3)) :
+        (~((hash << 11) + ((*str) ^ (hash >> 5))));
+    }
+
+    return hash;
 }
--- 3week/4/4/make_new_words.hpp
+++ 3week/4/4/make_new_words.hpp
@@ -1,13 +1,20 @@
-//
-//  make_new_words.hpp
-//  4
-//
-//  Created by Kate on 25.02.2021.
-//  Copyright © 2021 Kate. All rights reserved.
-//
-
 #ifndef make_new_words_h
 #define make_new_words_h
+
+std::set < std::string > make_random_words(std::size_t N, std::size_t length = 10)
+{
+    std::uniform_int_distribution <> letter(97, 122);
+    std::default_random_engine e(static_cast < std::size_t > (std::chrono::system_clock::now().time_since_epoch().count()));
+
+    std::set < std::string > words;
+
+    for (std::string s(length, '_'); words.size() < N; words.insert(s))
+    {
+        for (auto & c : s)
+            c = letter(e);
+    }
+    return words;
+}
 
 
 #endif /* make_new_words_h */
