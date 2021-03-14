0dd5ea3c7d4d0e4a4a152077eb71180df6d0102c
eparu <kat.pa@bk.ru>
2021-03-14 15:07:13 +0300

    output domen added

--- 5week/3/3/main.cpp
+++ 5week/3/3/main.cpp
@@ -8,13 +8,14 @@
 int main(int argc, char** argv)
 {
     std::string data = "e-mail address example@com, a..t@mail.ru, aa@mail.mipt.ru, aa.aa.aa@gmail.com, a.a.@mail.ru, a.a@mail.ru, .A.mail.ru, 89499, 9-0";
-    std::regex pattern(R"(\w+(?:\.\w+)*@\w+(?:\.\w+)*)");
+    std::regex pattern(R"((\w+(?:\.\w+)*)@(\w+(?:\.\w+)*))");
 
+    std::sregex_iterator begin(std::begin(data), std::end(data), pattern);
+    std::sregex_iterator end;
 
-    std::copy(
-              std::sregex_token_iterator(data.begin(), data.end(), pattern, { 0 }),
-              std::sregex_token_iterator(),
-              std::ostream_iterator < std::string >(std::cout, "\n"));
-
+    std::for_each(begin, end, [](const std::smatch& m)
+    {
+        std::cout << m[0] << " -- " << m[2] << std::endl;
+    });
     return 0;
 }
