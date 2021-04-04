2e3546e56f2ffed322c71082a34923d908bdcd15
eparu <kat.pa@bk.ru>
2021-04-04 20:29:23 +0300

    2 task added

--- 8week/2/2/main.cpp
+++ 8week/2/2/main.cpp
@@ -4,16 +4,17 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
-#include <chrono>
-#include <random>
 #include <thread>
 #include <vector>
+#include <random>
+#include <chrono>
 #include <map>
+
 
 std::string make_sequence(std::size_t N)
 {
-    std::uniform_int_distribution <> uid(0, 4);
-    std::default_random_engine dre(static_cast < std::size_t > (std::chrono::system_clock::now().time_since_epoch().count()));
+    std::uniform_int_distribution <> uid(0, 3);
+    std::default_random_engine dre(42);
     std::map<std::size_t, char> number_into_letter;
     number_into_letter[0] = 'A';
     number_into_letter[1] = 'G';
@@ -29,151 +30,83 @@ std::string make_sequence(std::size_t N)
 }
 
 
-class Threads_Guard
+template<typename T>
+void safe_push_back(std::vector<T>& vector, T value, std::mutex& mutex)
 {
-public:
-
-    explicit Threads_Guard(std::vector < std::thread > & threads) :
-    m_threads(threads)
-    {}
-
-    Threads_Guard(Threads_Guard const &) = delete;
-
-    Threads_Guard& operator=(Threads_Guard const &) = delete;
-
-    ~Threads_Guard() noexcept
-    {
-        try
-        {
-            for (std::size_t i = 0; i < m_threads.size(); ++i)
-            {
-                if (m_threads[i].joinable())
-                {
-                    m_threads[i].join();
-                }
-            }
-        }
-        catch (...)
-        {
-            // std::abort();
-        }
-    }
-
-private:
-
-    std::vector < std::thread > & m_threads;
-};
-
-
-template < typename Iterator >
-struct Searcher
-{
-    void operator()(Iterator first, Iterator last, Iterator first_search, Iterator last_search, std::promise < Iterator > & result, std::atomic < bool > & flag) noexcept
-    {
-        try
-        {
-            for (; (first != last) && (first_search != last_search) && !flag.load(); ++first, ++first_search)
-            {
-                if (*first != *first_search)
-                {
-                    return;
-                }
-            }
-            result.set_value(first);
-            flag.store(true);
-            return;
-        }
-        catch (...)
-        {
-            try
-            {
-                result.set_exception(std::current_exception());
-                flag.store(true);
-            }
-            catch (...)
-            {
-                // ...
-            }
-        }
-    }
-};
-
-template < typename Iterator >
-Iterator parallel_find(Iterator first, Iterator last, Iterator first_search, Iterator last_search )
-{
-    const std::size_t length = std::distance(first, last);
-
-    if (!length)
-        return last;
-
-    const std::size_t min_per_thread = 25;
-    const std::size_t max_threads =
-    (length + min_per_thread - 1) / min_per_thread;
-
-    const std::size_t hardware_threads =
-    std::thread::hardware_concurrency();
-
-    const std::size_t num_threads =
-    std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
-
-    const std::size_t block_size = length / num_threads;
-
-    std::atomic < std::size_t > result;
-    std::atomic < bool > flag(false);
-    std::vector < std::thread > threads(num_threads - 1);
-
-    {
-        Threads_Guard guard(threads);
-
-        Iterator block_start = first;
-
-        for (std::size_t i = 0; i < (num_threads - 1); ++i)
-        {
-            Iterator block_end = block_start;
-            std::advance(block_end, block_size);
-
-            threads[i] = std::thread(Searcher < Iterator>(),
-                                     block_start, block_end, first_search, last_search, std::ref(result), std::ref(flag));
-            block_start = block_end;
-        }
-
-        Searcher < Iterator> ()(block_start, last, first_search, last_search, result, flag);
-    }
-
-    if (!flag.load())
-    {
-        return last;
-    }
-
-    return result.get_future().get();
+    std::scoped_lock lock(mutex);
+    vector.push_back(value);
 }
 
-int main(int argc, char ** argv)
+
+struct searchSubstr
 {
-    constexpr std::size_t N = 10;
-    std::string big_string = make_sequence(N);
-    std::cout << big_string;
-    std::cout << "Enter string to search: ";
-
-    std::string string_to_search;
-    std::cin >> string_to_search;
-
-    auto result = parallel_find(big_string.begin(), big_string.end(), string_to_search.begin(), string_to_search.end());
-
-    if (result.size() != 0)
+    void operator()(const std::string & substr, const std::string & str,
+                    std::size_t first, std::size_t last, std::vector<std::size_t>& indices, std::mutex& mutex)
     {
-        std::cout << "Index number: ";
-        for (auto &i : result)
+        auto begin = std::begin(str);
+
+        for (auto index = first; index < last; ++index)
         {
-            std::cout << i << " ";
+            auto it1 = std::begin(substr);
+            auto it2 = std::next(begin, index);
+            for (; it1 != std::end(substr) && *it1 == *it2; ++it1, ++it2);
+            if (it1 == std::end(substr))
+            {
+                safe_push_back(indices, index, mutex);
+            }
         }
+    }
+};
+
+
+void parallelSearch(const std::string& substr, const std::string& str, std::size_t first_index, std::size_t last_index, std::vector<std::size_t>& indices, std::mutex& mutex)
+{
+    const std::size_t hardware_threads = std::thread::hardware_concurrency();
+    const unsigned int block_size =(str.size() - substr.size() + 1) /hardware_threads;
+
+    if ((last_index - first_index) <= std::max(4U,  block_size))
+    {
+        searchSubstr()(substr, str, first_index, last_index, indices, mutex);
     }
     else
     {
-        std::cout << "String not found" << std::endl;
-    }
+        auto middle_index = first_index + (last_index - first_index) / 2;
 
+        std::future < void > first_half = std::async(parallelSearch, std::cref(substr), std::cref(str), first_index, middle_index, std::ref(indices), std::ref(mutex));
+
+        parallelSearch(substr, str, middle_index, last_index, indices, mutex);
+
+        first_half.get();
+    }
+}
+
+void parallelSearchCall(const std::string& substr, const std::string& str, std::vector<std::size_t>& indices)
+{
+    std::mutex mutex;
+    parallelSearch(substr, str, 0U, str.size() - substr.size() + 1, indices, mutex);
+}
+
+int main()
+{
+
+    std::string str = make_sequence(50);
+    std::cout << str;
+    std::cout << "\nEnter substr to find: ";
+    std::string substr;
+    std::cin >> substr;
+
+    std::vector <std::size_t> ind;
+    parallelSearchCall(substr, str, ind);
+
+    std::sort(std::begin(ind), std::end(ind));
+
+    std::cout << "The beginning of substr located on positions: ";
+
+    for (auto i : ind)
+    {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
 
     return 0;
 }
-
