6b24e2556ab6dd73dd58872bafbb6a7ab5f52c7c
eparu <kat.pa@bk.ru>
2021-04-14 18:22:07 +0300

    create 10week folder

---
+++ 10week/task/task/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  task
+//
+//  Created by Kate on 14.04.2021.
+//  Copyright © 2021 Kate. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
